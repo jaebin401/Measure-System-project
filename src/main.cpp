ba9b7630de9c78d4c588c5aa6850b59d093c604b
jaebin401 <anjaebin200701@gmail.com>
2025-11-14 11:20:55 +0900

    버튼 기능 구현, 메인 프레임워크 제작

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,130 @@
 #include <Arduino.h>
-#include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); 
+#define BUTTON_A_PIN 2
+#define BUTTON_B_PIN 3
+#define BUZZER_PIN 9
 
-// button setting
-const int buttonPin = 2;
-const int buzzerPin = 8;
-int lastButtonState = HIGH;
-int currentButtonState = digitalRead(buttonPin);
+int mode = 0;
+const int TOTAL_MODES = 3;
 
-bool isClick()
-{
-  if (currentButtonState == LOW && lastButtonState == HIGH) {
-    Serial.println("버튼 눌림!"); 
+class DebouncedButton {
+    private:
+    uint8_t _pin;
+    long _debounceDelay;
+    long _lastDebounceTime;
+    int _buttonState;
+    int _lastButtonState;
 
-    digitalWrite(buzzerPin, HIGH);
-    delay(100);
-    digitalWrite(buzzerPin, LOW);
+    uint8_t _buzzerPin;
+    int _buzzerTone;
+    int _buzzerDuration;
+
+    public:
+    DebouncedButton(uint8_t pin) 
+    {
+        _pin = pin;
+        _debounceDelay = 50;
+        _lastDebounceTime = 0;
+        _buttonState = HIGH;
+        _lastButtonState = HIGH;
+        _buzzerPin = 0;
+        _buzzerTone = 0;
+        _buzzerDuration = 0;
+    }
+
+    void begin() 
+    {
+        pinMode(_pin, INPUT_PULLUP);
+    }
+
+    void attachBuzzer(uint8_t pin, int tone, int duration) 
+    {
+        _buzzerPin = pin;
+        _buzzerTone = tone;
+        _buzzerDuration = duration;
+        pinMode(_buzzerPin, OUTPUT);
+    }
+
+    bool checkPressed() 
+    {
+        bool pressedEvent = false;
+        int reading = digitalRead(_pin);
+
+        if (reading != _lastButtonState)
+        {
+            _lastDebounceTime = millis();
+        }
+
+        if ((millis() - _lastDebounceTime) > _debounceDelay) 
+        {
+            if (reading != _buttonState) 
+            {
+                _buttonState = reading;
+                
+                if (_buttonState == LOW) 
+                {
+                    pressedEvent = true; 
+                    if (_buzzerPin != 0) 
+                    {
+                        tone(_buzzerPin, _buzzerTone, _buzzerDuration);
+                    }
+                }
+            }
+        }
+        _lastButtonState = reading;
+        return pressedEvent;
+    }
+};
+
+DebouncedButton* buttonA;
+DebouncedButton* buttonB;
+
+void setup() {
+  Serial.begin(9600);
+  
+  buttonA = new DebouncedButton(BUTTON_A_PIN);
+  buttonB = new DebouncedButton(BUTTON_B_PIN);
+
+  buttonA->attachBuzzer(BUZZER_PIN, 1500, 100);
+  buttonB->attachBuzzer(BUZZER_PIN, 800, 100);
+
+  buttonA->begin();
+  buttonB->begin();
+
+  Serial.println("--- 버튼 뼈대 코드 (v3) ---");
+  Serial.print("현재 모드: ");
+  Serial.println(mode);
+}
+
+void loop() {
+  bool A_pressed = buttonA->checkPressed();
+  bool B_pressed = buttonB->checkPressed();
+
+  if (A_pressed) {
+    mode = (mode + 1) % TOTAL_MODES;
+    Serial.print("Button A! (UP) 현재 모드: ");
+    Serial.println(mode);
+  }
+
+  if (B_pressed) {
+    mode = mode - 1;
+    if (mode < 0) {
+      mode = TOTAL_MODES - 1;
+    }
+    Serial.print("Button B! (DOWN) 현재 모드: ");
+    Serial.println(mode);
+  }
+
+  switch (mode) {
+    case 0:
+      break;
+    case 1:
+      break;
+    case 2:
+      break;
+    default:
+      break;
   }
   
-  lastButtonState = currentButtonState;
-  return currentButtonState;
+  delay(10); 
 }
-
-void setup()
-{
-  // lcd setting
-  lcd.init();				
-  lcd.clear();         		
-  lcd.backlight(); 		
-  
-  lcd.setCursor(2,0); 		
-  lcd.print("hello world"); 
-
-  // button
-  pinMode(buzzerPin, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
-
-  // Serial Monitor Initialization
-  Serial.begin(9600);
-}
-
-void loop()
-{
-
-}
-
