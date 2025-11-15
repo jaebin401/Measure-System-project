502fe97866a79668d07179c4b0857130f5c91845
jaebin401 <anjaebin200701@gmail.com>
2025-11-15 13:01:28 +0900

    버튼 클릭 함수 초안 백업

---
+++ test/button_press_code.cpp
@@ -0,0 +1,25 @@
+/*
+
+void loop() 
+{
+  bool A_pressed = buttonA->checkPressed();
+  bool B_pressed = buttonB->checkPressed();
+
+  if (A_pressed) 
+  {
+    mode++;
+    Serial.print("Button A clicked, currne mode: ");
+    Serial.println(mode);
+    updateLcdDisplay();
+  }
+
+  if (B_pressed) 
+  {
+    mode--;
+    Serial.print("Button B clicked, current mode: ");
+    Serial.println(mode);
+    updateLcdDisplay();
+  }
+}
+
+*/
